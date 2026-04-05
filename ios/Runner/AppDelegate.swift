8dc0c6cb5615a4466925d4c335db08af8abccc73
mohamed <royal.academy1001@gmail.com>
2026-04-05 10:29:18 +0200

    fix ios build error

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,18 +1,13 @@
-import Flutter
 import UIKit
-import Firebase
+import Flutter
 
-@main
-@objc class AppDelegate: FlutterAppDelegate, FlutterImplicitEngineDelegate {
+@UIApplicationMain
+@objc class AppDelegate: FlutterAppDelegate {
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-
+    GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-  }
-
-  func didInitializeImplicitFlutterEngine(_ engineBridge: FlutterImplicitEngine) {
-    GeneratedPluginRegistrant.register(with: engineBridge.pluginRegistry)
   }
 }
